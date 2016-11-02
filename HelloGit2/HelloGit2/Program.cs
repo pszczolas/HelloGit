4486305b1c99fb46413d86284743608cd8694acc
pszczolas <mpszczolas@gmail.com>
2016-11-02 18:30:49 +0100

    Update Program.cs
    i've made change from github site

--- HelloGit2/HelloGit2/Program.cs
+++ HelloGit2/HelloGit2/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub 2");
+            Console.WriteLine("Hello from the other side");
         }
     }
 }
